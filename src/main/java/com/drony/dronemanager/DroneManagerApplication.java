a63a2845ba1d5a6dd1d6ccc39d3a4525fd30b172
marwa.goda <marwa.goda@qrios.se>
2023-08-14 14:03:54 +0200

    initial commit

---
+++ src/main/java/com/drony/dronemanager/DroneManagerApplication.java
@@ -0,0 +1,13 @@
+package com.drony.dronemanager;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DroneManagerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DroneManagerApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/drony/dronemanager/DroneManagerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.drony.dronemanager;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DroneManagerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
